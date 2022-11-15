096d98a4a9b699304fe1812928a5a49b1610c605
Warheart-1 <axel97477@gmail.com>
2022-11-15 13:04:35 +0100

    exo 2 symfony matin

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
 
 #[Route('/')]
 class Controller extends AbstractController
 {
+    public function __construct(protected ManagerRegistry $registry)
+    {
+    }
+
     #[Route('', name: 'home')]
     public function index(): Response
     {
-        return $this->render('base.html.twig');
+        $postregister = $this->registry->getRepository(Post::class);
+        $posts = $postregister->findA();
+
+        return $this->render('layout/index.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 }
---
+++ src/Entity/Post.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(?string $Content): self
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+}
